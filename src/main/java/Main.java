2aa25e49b00b786c461bce68c6d7641cadf7f04c
Alena Lipinskaya <lipinskya_alena@mail.ru>
2016-07-13 14:48:14 +0300

    AddMain

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+
+import javax.net.ssl.SSLSocketFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.net.ssl.SSLSocketFactory;
+
+
+public class Main {    
+	public static void main(String[] args) {		
+		new MyForm();
+	}
+}
